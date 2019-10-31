840b3408237f5752ff79155627560008290788fc
Haoran <562963269e@qq.com>
2019-10-31 22:58:01 +0800

    submit solution to 032

--- golang/_0032_Longest_Valid_Parentheses/main.go
+++ golang/_0032_Longest_Valid_Parentheses/main.go
@@ -1,14 +1,39 @@
 package main
 
-import (
-	"fmt"
-	"unicode/utf8"
-)
+import "fmt"
 
+// let dp[i+1] represent the length of longest valid parentheses ends with s[i]
+// then if s[i-1] == '(', dp[i+1] = dp[i-1] + 2
+//      if s[i-1-dp[i]] == '(', dp[i+1] = dp[i] + 2 + dp[j]
 func longestValidParentheses(s string) int {
-	return 0
+	var (
+		res int
+		n   = len(s)
+		dp  = make([]int, n+1)
+	)
+
+	if n > 1 {
+		for i := 1; i < n; i++ {
+			if s[i] == ')' {
+				if s[i-1] == '(' {
+					dp[i+1] = dp[i-1] + 2
+				} else {
+					j := i - 1 - dp[i]
+					if j >= 0 && s[j] == '(' {
+						dp[i+1] = dp[i] + 2 + dp[j]
+					}
+				}
+			}
+
+			if dp[i+1] > res {
+				res = dp[i+1]
+			}
+		}
+	}
+
+	return res
 }
 
 func main() {
-	fmt.Println(utf8.RuneCountInString("สอบทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบ ทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบทดสอบ🔴🔴🤣💫😆"))
+	fmt.Println(longestValidParentheses(")()())"))
 }
